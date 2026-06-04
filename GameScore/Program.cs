07c4f63cc10471a5d23cbd3424f14bde20a30ad0
emaGET <emanuelepini@getacademy.no>
2026-06-04 13:33:02 +0200

    games score practice

---
+++ GameScore/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GameScore;
+
+// {
+//
+//     public class ScoreList
+//     {
+//         private int[] _scores;
+//         private int _index;
+//
+//         public ScoreList()
+//         {
+//             _scores = new int[3];
+//             _index = 0;
+//         }
+//
+//         public void Add(int score)
+//         {
+//             _scores[_index] = score;
+//             _index++;
+//         }
+//     }
+// }
+
+class Program
+{
+    static void Main()
+    {
+        var list = new ScoreList();
+        list.Add(10);
+        list.Add(25);
+        list.Add(50);
+        list.Add(99);
+        list.Add(7);
+    }
+}
---
+++ GameScore/ScoreList.cs
@@ -0,0 +1,30 @@
+﻿namespace GameScore;
+        
+        public class ScoreList
+        {
+            
+            private int[] _scores;
+            private int _index;
+
+            public ScoreList()
+            {
+                _scores = new int[3];
+                _index = 0;
+            }
+
+            public void Add(int score)
+            {
+                
+                if (_index >= _scores.Length)
+                {
+                    var newScores = new int[_scores.Length * 2];
+                    Array.Copy(_scores, newScores, _scores.Length);
+                    _scores = newScores;
+                }
+                
+                _scores[_index] = score;
+                _index++;
+                
+            }
+            
+        }
