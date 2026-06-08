356376f5dcd8f06e2cf53735677efa9314f0a495
emaGET <emanuelepini@getacademy.no>
2026-06-08 10:53:43 +0200

    tested, arrange-act-assert, all went right

--- GameScore.Test/UnitTest1.cs
+++ GameScore.Test/UnitTest1.cs
@@ -10,6 +10,12 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var list = new ScoreList();
+        list.Add(10);
+        list.Add(25);
+
+        var summary = list.GetSummary();
+        
+        Assert.That(summary, Does.Contain("Player 1 scored 10"));
     }
 }
